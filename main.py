ec4a2796a80dc87abc84cf2701719cb9b471bd2d
Gamma7113131 <gametron2012@yahoo.com>
2024-11-23 15:57:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import os
 import conversion
 
-session = sa.login("TGammaMCoder","tgmao")
+session = sa.login("username","pwd")
 cloud = session.connect_scratch_cloud("1100152494")
 client = cloud.requests(no_packet_loss=True,respond_order="finish")
 
